f5fbd7b9651d3ef1998584f5e76651130f9bac0c
tg0xff <tg0xff@proton.me>
2023-09-13 18:28:19 -0600

    Define multiply()

--- main.js
+++ main.js
@@ -5,3 +5,7 @@ function add(summand1, summand2) {
 function subtract(minuend, subtrahend) {
   return minuend - subtrahend;
 }
+
+function multiply(factor1, factor2) {
+  return factor1 * factor2;
+}
