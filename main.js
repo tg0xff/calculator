a129b84309b5fcedf55013d3137d2c3599635748
tg0xff <tg0xff@proton.me>
2023-09-14 18:33:50 -0600

    Change styling when clicking a button

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const buttons = document.querySelectorAll("button");
+let backgroundColorContainer = "";
+let boxShadowContainer = "";
+
 function add(summand1, summand2) {
   return summand1 + summand2;
 }
@@ -13,3 +17,26 @@ function multiply(factor1, factor2) {
 function divide(dividend, divisor) {
   return dividend / divisor;
 }
+
+function styleWhileClicking(e) {
+  backgroundColorContainer = e.target.style["background-color"];
+  boxShadowContainer = e.target.style["box-shadow"];
+
+  e.target.style["background-color"] = "var(--background-colour)";
+  e.target.style["box-shadow"] = "none";
+}
+
+function styleAfterClicking(e) {
+  e.target.style["background-color"] = backgroundColorContainer;
+  e.target.style["box-shadow"] = boxShadowContainer;
+
+  backgroundColorContainer = "";
+  boxShadowContainer = "";
+}
+
+function styleButtonClick(button) {
+  button.addEventListener("mousedown", styleWhileClicking);
+  button.addEventListener("mouseup", styleAfterClicking);
+}
+
+buttons.forEach((button) => styleButtonClick(button));
